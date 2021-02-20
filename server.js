cd2d602b3754097549f85e74d1d743a642c3d8d8
Natan Stuart <nbs2115@gmail.com>
2021-02-19 18:17:03 -0800

    deploying

--- server.js
+++ server.js
@@ -13,10 +13,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
-{ useNewUrlParser: true, 
-useFindAndModify: false, 
-useUnifiedTopology: true});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 app.use(require("./routes/api-routes.js"));
 app.use(require("./routes/html-routes.js"));
